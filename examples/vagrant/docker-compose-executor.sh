6dae0cc2458ca9cef1eb3bd0efde0bd2c69ed5dc
Renan DelValle <rdelval1@binghamton.edu>
2016-08-08 18:08:24 -0700

    Modified the way in which Mesos and Marathon are installed. Updated documentation accordingly. \n\n Changed documentation to point to new sample tar.gz stored in bintray

--- examples/vagrant/docker-compose-executor.sh
+++ examples/vagrant/docker-compose-executor.sh
@@ -1,3 +1,2 @@
-COMPOSE_JAR_NAME=/home/vagrant/marathon/target/docker-compose-executor-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-COMPOSE_CLASS_NAME=com.paypal.mesos.executor.App
-java -cp ${COMPOSE_JAR_NAME} ${COMPOSE_CLASS_NAME}
+COMPOSE_JAR_NAME=/home/vagrant/marathon/target/docker-compose-executor_0.0.1.jar
+java -jar ${COMPOSE_JAR_NAME}
